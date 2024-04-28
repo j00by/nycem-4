cd891db482ec4cf6688eff4a9b8bcbb2c76bee8f
j00by <judy.h@nyu.edu>
2024-04-28 17:31:13 -0400

    update staten island from circles to custom markers

--- scripts.js
+++ scripts.js
@@ -199,7 +199,7 @@ const config = {
                 easing: (t) => t * (2 - t) // easing function for a more gentle animation
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: true,
+            rotateAnimation: false,
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
